3b6f20a1ff05c03e56b655ff2324b2d83d1bf481
DeoxysTheGod <quentinb2004@gmail.com>
2023-10-04 12:10:46 +0200

    UI implementation

--- ressources/R3/R3.04 - Qualite de Dev/TD/td1/src/main/java/net/quentin/LibraryUI.java
+++ ressources/R3/R3.04 - Qualite de Dev/TD/td1/src/main/java/net/quentin/LibraryUI.java
@@ -1,15 +1,22 @@
 package net.quentin;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LibraryUI {
+
+    private ArrayList<Library> libraries;
+
+    public LibraryUI(ArrayList<Library> libraries) {
+        this.libraries = libraries;
+    }
 
     public boolean mainChoice() {
         int answer = UserInput.displayIntChoice("What do you want ? : ",
                 "Create Library", "Access to a library", "exit");
 
         if (answer == 0) {
-            System.out.println("do something");
+            createLibrary();
             return true;
         } else if (answer == 1) {
             System.out.println("do another thing");
@@ -17,6 +24,18 @@ public class LibraryUI {
         } else {
             return false;
         }
+    }
+
+    public void createLibrary() {
+        System.out.println("Choose the name of the library : ");
+        String name = UserInput.sc.next();
+        System.out.println("Choose the address of the library : ");
+        String address = UserInput.sc.next();
+        libraries.add(new Library(name, address));
+    }
+
+    public void accessToALibrary() {
+
     }
 
     public void launch() {
